471b9169019d9910b082cb2ac30d2fd7c813a252
HenriqueMonteiro <Henriquepupet@gmail.com>
2023-03-02 17:18:18 +0000

    Added comentary

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -2,8 +2,10 @@
 
 namespace OlaMundo
 {
+    /// Indicates the class, that is Program
     class Program
     {
+        ///Main with no return
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
