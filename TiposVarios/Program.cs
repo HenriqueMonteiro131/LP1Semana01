70c01982d7653a42c302fc32b595412d04d55256
HenriqueMonteiro <Henriquepupet@gmail.com>
2023-03-02 23:36:37 +0000

    Added Normal variables and it shows their value on screen

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Banana = 8;
+            int Kiwi = 5;
+            int Apple = 9;
+            int Peach = 3;
+            int Grape = 12;
+            
+            {
+                Console.WriteLine(Banana);
+                Console.WriteLine(Kiwi);
+                Console.WriteLine(Apple);
+                Console.WriteLine(Peach);
+                Console.WriteLine(Grape);
+
+            }
+                
+        }
+    }
+}
