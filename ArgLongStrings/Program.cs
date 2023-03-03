2c8d22ae03da7cff9631e0d2f3e35f24259897cf
HenriqueMonteiro <Henriquepupet@gmail.com>
2023-03-03 14:51:53 +0000

    Added 4 letter check also Caps lock transformer

---
+++ ArgLongStrings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ArgLongStrings
+{
+    class Program
+    {
+        private const int V = 4;
+
+        static void Main(string[] args)
+        {
+
+            // is empty?
+            // if -> ir por todos os argumentos e verificar o tamanho
+            for(int i =0; i<args.Length;i++ )
+            {
+                if(args[i].Length > 4)
+                Console.WriteLine(args[i].ToUpper());
+            }
+
+        }
+    }
+}
