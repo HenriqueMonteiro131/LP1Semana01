6a83694e07540ac3b20bf03158d9c7d1e7aa6e40
HenriqueMonteiro <Henriquepupet@gmail.com>
2023-03-03 00:39:18 +0000

    Added Char

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -11,6 +11,7 @@ namespace TiposVarios
             int Apple = 9;
             int Peach = 3;
             int Grape = 12;
+            char newLine = '\u2708';
             
             {
                 Console.WriteLine(Banana);
@@ -18,6 +19,7 @@ namespace TiposVarios
                 Console.WriteLine(Apple);
                 Console.WriteLine(Peach);
                 Console.WriteLine(Grape);
+                Console.WriteLine(newLine);
 
             }
                 
